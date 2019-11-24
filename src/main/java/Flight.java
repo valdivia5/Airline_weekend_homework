aa747dfda5088d354b6b1a46d3c50ffc35f68a67
usuario <usuario@mbp-de-usuario.mynet>
2019-11-24 22:14:53 +0000

    flight has got passengers, plane, number, destination, departure airport and departure time

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,21 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Flight {
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String number;
+    private String destination;
+    private String departureAirport;
+    private LocalDateTime departureTime;
+
+
+    public Flight(Plane plane, String number, String destination, String departureAirport, LocalDateTime departureTime) {
+        this.passengers = new ArrayList<Passenger>();
+        this.plane = plane;
+        this.number = number;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
 }
