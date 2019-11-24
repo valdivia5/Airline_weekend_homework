8f16d218b217596dc602a9b4ce149e35b71fa245
usuario <usuario@mbp-de-usuario.mynet>
2019-11-24 11:00:23 +0000

    passenger has name and luggage

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,21 @@
 public class Passenger {
+    private String name;
+    private int luggage;
+    private Flight flight;
+    private int seatNumber;
+
+    public Passenger(String name, int luggage) {
+        this.name = name;
+        this.luggage = luggage;
+        this.flight = null;
+        this.seatNumber = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLuggage() {
+        return this.luggage;
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void setup() {
+        passenger = new Passenger("Adrian", 2);
+    }
+
+    @Test
+    public void passengerHasName() {
+        assertEquals("Adrian", passenger.getName());
+    }
+    @Test
+    public void pasengerHasLuggage(){
+        assertEquals(2, passenger.getLuggage());
+    }
 }
